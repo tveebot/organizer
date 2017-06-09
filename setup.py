6efbad67ee803ac666d077d9cee55bcca8d79cd7
David Fialho <fialho.david@protonmail.com>
2017-06-09 18:20:33 +0100

    Added separate packages for unit and integration tests

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='show_organizer',
-    version='v0.1',
+    version='0.1',
     description='Tool to organize video files corresponding to TV Shows',
     url='https://github.com/davidfialho14/show_organizer',
     license='MIT',
     author='david',
     author_email='fialho.david@protonmail.com',
 
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=['tests', 'tests.unit', 'tests.integration']),
 
     extras_require={
         'test': ['pytest'],
