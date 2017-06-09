02ef2756a5fd14e4482517882bab098e5ceae262
David Fialho <fialho.david@protonmail.com>
2017-06-09 17:54:38 +0100

    Added documentation to the mapper module

--- show_organizer/mapper.py
+++ show_organizer/mapper.py
@@ -1,16 +1,25 @@
 import re
 
 from show_organizer.episode import Episode
+from show_organizer.tvshow import TVShow
 
 # An episode pattern corresponds to a 'word' of the form S01E01
 # Where S01 indicates the episode corresponds to season 1
 # and E01 indicates the episode is the first episode of the season
-from show_organizer.tvshow import TVShow
-
 episode_pattern = re.compile('S(?P<season>\d+)E(?P<number>\d+)\Z')
 
 
 def map_to_episode(name: str) -> Episode:
+    """
+    Maps a name to an episode object. The input name maybe the name of a folder or a file. The name is expected have a
+    format similar to 'TV.Show.Name.S01E02.720p', where the substring 'TV.Show.Name' indicates the name of the
+    episode's tv show and 'S01E02' indicates that the episode is the second of season 1. Characters to the right of
+    'S01E01' are all ignored.
+
+    :param name: name to be parsed and mapped to an episode.
+    :return: the episode corresponding to the given name.
+    :raise ValueError: if the name can not be mapped to an episode.
+    """
 
     # The character '.' divides words
     # Take each word
