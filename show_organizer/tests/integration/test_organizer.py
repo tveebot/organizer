4ba555d582e727fb8243340218438aa2a3dab567
David Fialho <fialho.david@protonmail.com>
2017-06-10 11:06:19 +0100

    Added documentation to the organizer
    video file is now called episode file

--- show_organizer/tests/integration/test_organizer.py
+++ show_organizer/tests/integration/test_organizer.py
@@ -18,41 +18,41 @@ class TestOrganizer:
         return tmpdir.mkdir("WATCH_DIR")
 
     @pytest.fixture
-    def video_file(self, watch_dir):
-        video_file = watch_dir.join("Prison.Break.S05E09.mkv")
-        video_file.write("")  # actually create the file
-        return str(video_file)
+    def episode_file(self, watch_dir):
+        episode_file = watch_dir.join("Prison.Break.S05E09.mkv")
+        episode_file.write("")  # actually create the file
+        return str(episode_file)
 
-    def test_store_EpisodeDirectoryAlreadyExists_MoveTheFileThere(self, storage_dir, video_file):
+    def test_store_EpisodeDirectoryAlreadyExists_MoveTheFileThere(self, storage_dir, episode_file):
         organizer = Organizer(str(storage_dir))
         storage_dir.mkdir("Prison Break").mkdir("Season 05")
 
-        organizer.store(video_file, Episode(TVShow("Prison Break"), season=5, number=9))
+        organizer.store(episode_file, Episode(TVShow("Prison Break"), season=5, number=9))
 
         assert os.path.exists(os.path.join(str(storage_dir), "Prison Break/Season 05/Prison.Break.S05E09.mkv"))
-        assert not os.path.exists(video_file)
+        assert not os.path.exists(episode_file)
 
-    def test_store_OnlyTVShowDirectoryExists_CreateSeasonDirectoryAnMoveTheFileThere(self, storage_dir, video_file):
+    def test_store_OnlyTVShowDirectoryExists_CreateSeasonDirectoryAnMoveTheFileThere(self, storage_dir, episode_file):
         organizer = Organizer(str(storage_dir))
         storage_dir.mkdir("Prison Break")
 
-        organizer.store(video_file, Episode(TVShow("Prison Break"), season=5, number=9))
+        organizer.store(episode_file, Episode(TVShow("Prison Break"), season=5, number=9))
 
         assert os.path.exists(os.path.join(str(storage_dir), "Prison Break/Season 05/Prison.Break.S05E09.mkv"))
-        assert not os.path.exists(str(video_file))
+        assert not os.path.exists(str(episode_file))
 
-    def test_store_EpisodeDirectoryDoesNotExist_CreateItAndMoveTheFileThere(self, storage_dir, video_file):
+    def test_store_EpisodeDirectoryDoesNotExist_CreateItAndMoveTheFileThere(self, storage_dir, episode_file):
         organizer = Organizer(str(storage_dir))
 
-        organizer.store(str(video_file), Episode(TVShow("Prison Break"), season=5, number=9))
+        organizer.store(str(episode_file), Episode(TVShow("Prison Break"), season=5, number=9))
 
         assert os.path.exists(os.path.join(str(storage_dir), "Prison Break/Season 05/Prison.Break.S05E09.mkv"))
-        assert not os.path.exists(str(video_file))
+        assert not os.path.exists(str(episode_file))
 
-    def test_store_FileAlreadyExistsOnDestinationDirectory_RaiseFileExistsError(self, storage_dir, video_file):
+    def test_store_FileAlreadyExistsOnDestinationDirectory_RaiseFileExistsError(self, storage_dir, episode_file):
         organizer = Organizer(str(storage_dir))
         storage_dir.mkdir("Prison Break").mkdir("Season 05").join("Prison.Break.S05E09.mkv").write("")
 
         with pytest.raises(FileExistsError,
                            message="File 'Prison.Break.S05E09.mkv' already exists in 'Prison Break/Season 05/'"):
-            organizer.store(str(video_file), Episode(TVShow("Prison Break"), season=5, number=9))
+            organizer.store(str(episode_file), Episode(TVShow("Prison Break"), season=5, number=9))
