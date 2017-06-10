2f6cfcddf466cbc00fef871d1f08af994410c8e8
David Fialho <fialho.david@protonmail.com>
2017-06-10 14:04:03 +0100

    Added documentation for the watcher

--- show_organizer/watch_handler.py
+++ show_organizer/watch_handler.py
@@ -2,18 +2,30 @@ from watchdog.events import FileSystemEventHandler, FileSystemEvent
 
 
 class WatchHandler:
+    """
+    WatchHandler defines an interface with two methods: on_new_file and on_new_directory.
+    These methods are called by the watcher once a new file or a new directory is detected on the watch directory.
+    Classes that implement this interface should implement this methods to determine the actions to be taken once a
+    new file/directory is detected.
+    """
 
     def __init__(self):
         self.base_handler = _BaseHandler(self)
 
     def on_new_file(self, file_path):
+        """ Called once a new file is detected on the watch directory """
         pass
 
     def on_new_directory(self, dir_path):
+        """ Called once a new directory is detected on the watch directory """
         pass
 
 
 class _BaseHandler(FileSystemEventHandler):
+    """
+    This is just a wrapper around a FileSystemEventHandler to call the correct watch handler methods once a new file
+    or directory are detected.
+    """
 
     def __init__(self, watch_handler):
         self.watch_handler = watch_handler
