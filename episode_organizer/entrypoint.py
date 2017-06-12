aa2cfaecb1bbaf8be6680eec6c342dccb9173e38
David Fialho <fialho.david@protonmail.com>
2017-06-12 14:57:25 +0100

    Refactored the entrypoint
    The entrypoint is now a class
    The various steps in setting up the service are now compartmentalized into multiple methods

--- episode_organizer/entrypoint.py
+++ episode_organizer/entrypoint.py
@@ -10,11 +10,12 @@ Options:
   --version     Show version.
 
 """
+import logging
 import os
 import sys
-import logging
 from configparser import ConfigParser
 from logging.config import fileConfig
+
 from docopt import docopt
 from pkg_resources import resource_filename, Requirement
 
@@ -24,86 +25,114 @@ from episode_organizer.organizer import Organizer
 from episode_organizer.storage_manager import StorageManager
 
 
-def main():
-    args = docopt(__doc__, version='TV Show Organizer Version 0.1')
+class EntryPoint:
 
-    # Load configurations
+    DEFAULT_CONFIG_FILE_LOCATION = os.path.join(os.getenv("HOME"), ".config", "episode_organizer")
+    DEFAULT_CONFIG_FILE = os.path.join(DEFAULT_CONFIG_FILE_LOCATION, "config.ini")
+
+    config_file = DEFAULT_CONFIG_FILE
     config = ConfigParser()
-
-    # Always load the default configuration file
-    # The configurations in this file are overridden by the ones defined in the configuration file provided
-    # by the user, if the user provides one.
-    default_config = resource_filename(Requirement.parse("episode_organizer"), 'episode_organizer/default.ini')
-    config.read(default_config)
-
-    if args['--conf']:
-        # User provided a configuration file
-        config_file = args['--conf']
-
-        if not os.path.isfile(config_file):
-            print("Error: config file '%s' does not exist" % config_file, file=sys.stderr)
-            sys.exit(1)
-
-        # Override configurations in the default conf file
-        config.read(config_file)
-
-    # Setup loggers
-
-    if args['--logs']:
-        # User provided a config file for the logger
-        logs_config_file = args['--logs']
-
-        if not os.path.isfile(logs_config_file):
-            print("Error: logs config file '%s' does not exist" % logs_config_file, file=sys.stderr)
-            sys.exit(1)
-
-        fileConfig(logs_config_file)
-
-    else:
-        # Use the default
-        fileConfig(default_config)
-
     logger = logging.getLogger()
+    organizer = None
+    configurator = None
 
-    # Validate configurations: check if both the watch and storage directories exist
+    def main(self):
+        args = docopt(__doc__, version='TV Show Organizer Version 0.1')
 
-    watch_dir = config['DEFAULT']['WatchDirectory']
-    storage_dir = config['DEFAULT']['StorageDirectory']
+        self.load_configurations(config_file=args['--conf'])
+        self.setup_loggers(logs_config_file=args['--logs'])
+        self.setup_organizer()
 
-    if not os.path.isdir(watch_dir):
-        logger.error("Watch directory does not exist: %s" % watch_dir)
-        sys.exit(1)
+        try:
+            self.start_services()
+            self.serve_forever()
 
-    if not os.path.isdir(storage_dir):
-        logger.error("Storage directory does not exist: %s" % storage_dir)
-        sys.exit(1)
+        except KeyboardInterrupt:
+            self.stop_services()
 
-    # Configure the organizer
-    episode_filter = Filter()
-    mapper = Mapper()
-    storage_manager = StorageManager(storage_dir)
+        except Exception:
+            self.logger.exception("Caught an unexpected exception")
+            self.stop_services()
 
-    organizer = Organizer(watch_dir, episode_filter, mapper, storage_manager)
+    def load_configurations(self, config_file):
 
-    try:
-        # Start the service in a thread and wait until it terminates
-        organizer.start()
-        logger.info("Started organizing service")
+        # Always load the default configuration file
+        # The configurations in this file are overridden by the ones defined in the configuration file provided
+        # by the user, if the user provides one.
+        default_config = resource_filename(Requirement.parse("episode_organizer"), 'episode_organizer/default.ini')
 
-        organizer.join()
+        self.config.read(default_config)
 
-    except KeyboardInterrupt:
-        logger.info("Stopping the service...")
-        organizer.stop()
+        if config_file:
 
-    except Exception:
-        logger.exception("Caught an unexpected exception")
+            # User provided a path for a configuration file
+            # Changes to the configurations will be updated to this file
+            self.config_file = config_file
 
-        logger.info("Stopping the service...")
-        organizer.stop()
+            if os.path.isfile(config_file):
+                # Override configurations in the default conf file
+                self.config.read(config_file)
+            else:
+                self.logger.warning("Config file does not exist yet: using default configurations")
 
-    logger.info("Service was stopped")
+        else:
+            # Use the config file in the default location
+            # Changes to the configurations will be updated to this file
+            os.makedirs(self.DEFAULT_CONFIG_FILE_LOCATION, exist_ok=True)
+            self.config_file = self.DEFAULT_CONFIG_FILE
 
+    def setup_loggers(self, logs_config_file):
+
+        if logs_config_file:
+
+            # User provided a config file for the loggers
+            # Will use this configuration
+            if not os.path.isfile(logs_config_file):
+                self.logger.error("The loggers file indicated does not exist: %s" % logs_config_file)
+                sys.exit(1)
+
+            fileConfig(logs_config_file)
+
+        else:
+            # Use the default
+            fileConfig(self.DEFAULT_CONFIG_FILE)
+
+        self.logger = logging.getLogger()
+
+    def setup_organizer(self):
+
+        # Check if both the watch and storage directories exist
+        watch_dir = self.config['DEFAULT']['WatchDirectory']
+        storage_dir = self.config['DEFAULT']['StorageDirectory']
+
+        if not os.path.isdir(watch_dir):
+            self.logger.error("Watch directory does not exist: %s" % watch_dir)
+            sys.exit(1)
+
+        if not os.path.isdir(storage_dir):
+            self.logger.error("Storage directory does not exist: %s" % storage_dir)
+            sys.exit(1)
+
+        # Setup each component
+        episode_filter = Filter()
+        mapper = Mapper()
+        storage_manager = StorageManager(storage_dir)
+
+        self.organizer = Organizer(watch_dir, episode_filter, mapper, storage_manager)
+
+    def start_services(self):
+        self.logger.info("Starting organizer service...")
+        self.organizer.start()
+
+    def serve_forever(self):
+        self.logger.info("Running...")
+        self.organizer.join()
+
+    def stop_services(self):
+        self.logger.info("Stopping the organizer service...")
+        self.organizer.stop()
+
+        self.logger.info("All services were stopped")
 
 if __name__ == '__main__':
-    main()
+    EntryPoint().main()
