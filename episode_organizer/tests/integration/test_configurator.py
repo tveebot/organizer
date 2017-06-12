a08c950ce4f0476816e3857fd7e2c2de0d721b83
David Fialho <fialho.david@protonmail.com>
2017-06-12 12:35:13 +0100

    Refactored some of Configurator's integration tests

--- episode_organizer/tests/integration/test_configurator.py
+++ episode_organizer/tests/integration/test_configurator.py
@@ -34,7 +34,7 @@ class TestConfigurator:
         organizer.join()
 
     @patch.object(Organizer, 'on_new_file')
-    def test_SetWatchDirToAnExistingDir(self, on_new_file_mock, tmpdir):
+    def test_SetWatchDirToExistingDir_ChangesToNewDirAreDetected(self, on_new_file_mock, tmpdir):
 
         watch_dir = tmpdir.mkdir("watch")
         storage_dir = tmpdir.mkdir("storage")
@@ -49,21 +49,14 @@ class TestConfigurator:
             # The client tries to change the watch directory to an existing directory
             client.set_watch_dir(str(new_watch_dir))
 
-            # Check the server changed the watch directory
-            assert client.watch_dir() == str(new_watch_dir)
-
             # Someone creates a new file in the new watch directory
             new_watch_dir.join("file.txt").write("")
 
-        # Verify that the watcher detected the new created file
+        # New file in the new watch directory is detected
         on_new_file_mock.assert_called_once_with(new_watch_dir.join("file.txt"))
 
-        # And configurator logs the change
-        configurator_logger_mock.info\
-            .assert_called_once_with("Watch directory was changed to: %s" % str(new_watch_dir))
-
     @patch.object(Organizer, 'on_new_file')
-    def test_SetWatchDirToAnNonExistingDir(self, on_new_file_mock, tmpdir):
+    def test_SetWatchDirToNonExistingDir_ChangesToPreviousDirAreDetected(self, on_new_file_mock, tmpdir):
 
         watch_dir = tmpdir.mkdir("watch")
         storage_dir = tmpdir.mkdir("storage")
@@ -75,18 +68,14 @@ class TestConfigurator:
             # A client connects to the configurator
             client = ServerProxy('http://localhost:8000', allow_none=True)
 
-            with pytest.raises(Fault) as exception_info:
-                # The client tries to change the watch directory to an NON-existing directory
+            # The client tries to change the watch directory to an NON-existing directory, which will raise an error
+            with pytest.raises(Fault):
                 client.set_watch_dir(str(new_watch_dir))
 
-            # Check that server raised FileNotFoundError and it did not change the work directory
-            assert "class 'FileNotFoundError'" in str(exception_info.value)
-            assert client.watch_dir() == str(watch_dir)
-
-            # Someone creates a new file in the previous watch directory
+            # Afterwards, someone creates a new file in the previous watch directory
             watch_dir.join("file.txt").write("")
 
-        # The watcher detects the new file because it is still watching the same directory
+        # New file in the previous watch directory is detected
         on_new_file_mock.assert_called_once_with(watch_dir.join("file.txt"))
 
         # And the configurator logged a warning indicating the error
@@ -94,7 +83,7 @@ class TestConfigurator:
             .assert_called_once_with("Tried changing watch directory to '%s', but that directory did not exist. "
                                      "Kept previous watch directory" % str(new_watch_dir))
 
-    def test_SetStorageDirToAnExistingDir_EpisodeFilesAreStoredInTheNewDir(self, tmpdir):
+    def test_SetStorageDirToExistingDir_EpisodeFilesAreStoredInTheNewDir(self, tmpdir):
 
         watch_dir = tmpdir.mkdir("watch")
         storage_dir = tmpdir.mkdir("storage")
@@ -109,9 +98,6 @@ class TestConfigurator:
             # The client tries to change the storage directory to an existing directory
             client.set_storage_dir(str(new_storage_dir))
 
-            # Check the server changed the storage directory
-            assert client.storage_dir() == str(new_storage_dir)
-
             # Someone creates a new episode file in the watch directory
             watch_dir.join("Prison.Break.S05E09.720p.HDTV.x264.mkv").write("")
 
@@ -123,7 +109,7 @@ class TestConfigurator:
         configurator_logger_mock.info\
             .assert_called_once_with("Storage directory was changed to: %s" % str(new_storage_dir))
 
-    def test_SetStorageDirToANonExistingDir_ClientReceivesFileNotFoundError(self, tmpdir):
+    def test_SetStorageDirToNonExistingDir_EpisodeFilesAreStillMovedIntoPreviousDir(self, tmpdir):
 
         watch_dir = tmpdir.mkdir("watch")
         storage_dir = tmpdir.mkdir("storage")
@@ -131,21 +117,18 @@ class TestConfigurator:
 
         configurator_logger_mock = MagicMock()
         with self.setup_system(str(watch_dir), str(storage_dir), configurator_logger_mock):
+
             # A client connects to the configurator
             client = ServerProxy('http://localhost:8000', allow_none=True)
 
-            with pytest.raises(Fault) as exception_info:
-                # The client tries to change the storage directory to an NON-existing directory
+            # The client tries to change the storage directory to an NON-existing directory, which will raise an error
+            with pytest.raises(Fault):
                 client.set_storage_dir(str(new_storage_dir))
-
-            # Check that server raised FileNotFoundError and it did not change the work directory
-            assert "class 'FileNotFoundError'" in str(exception_info.value)
-            assert client.storage_dir() == str(storage_dir)
 
             # Someone creates a new episode file in the watch directory
             watch_dir.join("Prison.Break.S05E09.720p.HDTV.x264.mkv").write("")
 
-        # The episode file was moved into the previous storage directory
+        # The episode file was still moved into the previous storage directory
         assert storage_dir.join("Prison Break").join("Season 05").join("Prison.Break.S05E09.720p.HDTV.x264.mkv").check()
 
         # And the configurator logged a warning indicating the error
