41bb78f14f13064258551042addd64dd4f563994
David Fialho <fialho.david@protonmail.com>
2017-06-13 12:34:12 +0100

    Client now actually connects to the configurator service

--- episode_organizer/config_client/config_client.py
+++ episode_organizer/config_client/config_client.py
@@ -1,3 +1,6 @@
+from xmlrpc.client import ServerProxy
+
+
 class ConfigClient:
 
     def __init__(self, server_address):
@@ -7,12 +10,14 @@ class ConfigClient:
             'StorageDirectory': self.set_storage_dir,
         }
 
+        self._configurator = ServerProxy('http://%s:%s' % (server_address[0], server_address[1]), allow_none=True)
+
     def set_config(self, key, value):
         set_method = self._config_methods[key]
         set_method(value)
 
     def set_watch_dir(self, value):
-        pass
+        self._configurator.set_watch_dir(value)
 
     def set_storage_dir(self, value):
-        pass
+        self._configurator.set_storage_dir(value)
