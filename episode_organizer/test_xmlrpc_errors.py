4f34bf535a393d5635cccc8da1dfd0f69efd128d
David Fialho <fialho.david@protonmail.com>
2017-06-14 11:25:51 +0100

    Added decorator function to handle the conversion from raised errors to Fault exceptions

--- episode_organizer/test_xmlrpc_errors.py
+++ episode_organizer/test_xmlrpc_errors.py
@@ -1,6 +1,13 @@
+from xmlrpc.client import Fault
+
 import pytest
 
-from episode_organizer.xmlrpc_errors import error_code, error
+from episode_organizer.xmlrpc_errors import error_code, error, raise_faults
+
+
+class UnexpectedException(Exception):
+    """ Represents an exception that is not defined in the expected errors """
+    pass
 
 
 def test_error_code_ProvidesExpectedErrorClass_ReturnsCorrespondingCode():
@@ -12,9 +19,6 @@ def test_error_code_ProvidesExpectedErrorClassInstance_ReturnsCorrespondingCode(
 
 
 def test_error_code_ProvidesUnexpectedErrorClass_RaisesKeyError():
-
-    class UnexpectedException(Exception):
-        pass
 
     with pytest.raises(KeyError):
         error_code(UnexpectedException)
@@ -28,3 +32,34 @@ def test_error_ProvidesInvalidErrorCode_RaisesKeyError():
 
     with pytest.raises(KeyError):
         error(1)
+
+
+def test_raise_faults_FunctionRaisesExpectedError_RaisesFaultWithCorrespondingCodeAndMessage():
+
+    @raise_faults
+    def raising_func():
+        raise FileNotFoundError("error message")
+
+    with pytest.raises(Fault) as exception_info:
+        raising_func()
+
+    exception_info.value.faultCode = 1001
+    exception_info.value.faultString = "error message"
+
+
+def test_raise_faults_FunctionDoesNotRaiseException_DoesNotRaiseException():
+
+    @raise_faults
+    def non_raising_func():
+        pass  # does not raise anything
+
+    non_raising_func()
+
+
+def test_raise_faults_FunctionRaisesUnexpectedError_RaisesTheUnexpectedError():
+    @raise_faults
+    def raising_func():
+        raise UnexpectedException("error message")
+
+    with pytest.raises(UnexpectedException):
+        raising_func()
--- episode_organizer/xmlrpc_errors.py
+++ episode_organizer/xmlrpc_errors.py
@@ -1,4 +1,6 @@
+from functools import wraps
 from inspect import isclass
+from xmlrpc.client import Fault
 
 from bidict import bidict
 
@@ -20,4 +22,28 @@ def error_code(error):
 def error(error_code):
     """ Returns the error class corresponding to the given code """
     return _error_codes.inv[error_code]
+
+
+def raise_faults(func):
+    """
+    This is supposed to be used as a decorator.
+    All the exceptions raised by the decorated function are converted to Fault exceptions. If the raised error is
+    included in the 'error_codes' list, then fault code will be set to the error code of that error. Otherwise,
+    the exception is just re-raised and we let the xmlrpc library convert the exception to a Fault.
+    """
+    @wraps(func)
+    def exception_to_fault(*args, **kwargs):
+
+        try:
+            return func(*args, **kwargs)
+        except Exception as error:
+
+            try:
+                code = error_code(error)
+                raise Fault(faultCode=code, faultString=str(error))
+
+            except KeyError:
+                raise error
+
+    return exception_to_fault
 
