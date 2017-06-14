9ca7a27068708b3427a2b3b36eafe76c74518136
David Fialho <fialho.david@protonmail.com>
2017-06-14 19:11:34 +0100

    Added and implemented the Configuration class

---
+++ episode_organizer/daemon/configuration.py
@@ -0,0 +1,59 @@
+import os
+from configparser import ConfigParser
+from contextlib import suppress
+
+from pkg_resources import resource_filename, Requirement
+
+
+class Configuration:
+    """ The configuration class provides an interface to access and modify the current configurations of the system """
+
+    default_config_file = resource_filename(
+        Requirement.parse("episode_organizer"), 'episode_organizer/daemon/default.ini')
+
+    def __init__(self, config_file):
+        """
+        Initializes the configurations with the default values from the 'default.ini' file.
+        If the provided config file exists then the configurations from that file will be used.
+        """
+        self.config_file = config_file
+        self.config = ConfigParser()
+        self.config.read(self.default_config_file)
+
+    def __getitem__(self, key):
+        """
+        Returns the current value for a configuration key. If the configuration file does not exist, then returns the
+        default value for the given key.
+
+        :param key: the configuration key to obtain the value of.
+        :return: the value for the given key.
+        :raise KeyError: if the given key is invalid.
+        :raise OSError:  if it fails to read the config file.
+        """
+        # Note: should not use read(file) below because this ignores any OS error when trying to read the file
+        # Here we do want to ignore FileNotFoundError, but not other OS errors such as permission denied or others.
+        # We want to make sure that if the file exists, then we are able to read it.
+        with suppress(FileNotFoundError):
+            with open(self.config_file) as file:
+                self.config.read_file(file)  # do not user read() here (see note above)
+
+        return self.config['DEFAULT'][key]
+
+    def __setitem__(self, key, value):
+        """
+        Sets a new value for a valid configuration key. Updates the configuration file.
+
+        :param key:                 configuration key to set value for.
+        :param value:               value to set.
+        :raise KeyError:            if the provided key is not valid.
+        :raise OSError:             if it fails to write to the config file.
+        :raise FileNotFoundError:   if it failed to create the config file because its directory does not exist.
+        """
+        if key not in self.config['DEFAULT']:
+            raise KeyError
+
+        self.config['DEFAULT'][key] = value
+
+        # Update the configuration file
+        with open(self.config_file, "w") as file:
+            self.config.write(file)
---
+++ episode_organizer/tests/unit/test_configuration.py
@@ -0,0 +1,158 @@
+from configparser import ConfigParser
+
+import pytest
+
+from episode_organizer.daemon.configuration import Configuration
+from episode_organizer.tests import defaults
+
+
+class TestConfiguration:
+
+    @staticmethod
+    def config_file_contains(config_file, key, value):
+        config = ConfigParser()
+        config.read(config_file)
+
+        try:
+            current_value = config['DEFAULT'][key]
+            return current_value == value
+
+        except KeyError:
+            return False
+
+    def test_GetValidKey_ReturnsCorrespondingValue(self, tmpdir):
+
+        config_file = tmpdir.join("config.ini")
+        config_file.write(
+            "[DEFAULT]\n"
+            "WatchDirectory = my_watch\n"
+        )
+
+        config = Configuration(str(config_file))
+
+        assert config['WatchDirectory'] == "my_watch"
+
+    def test_GetInvalidValidKey_ReturnsKeyError(self, tmpdir):
+
+        config_file = tmpdir.join("config.ini")
+        config_file.write(
+            "[DEFAULT]\n"
+            "WatchDirectory = my_watch\n"
+        )
+
+        config = Configuration(str(config_file))
+
+        with pytest.raises(KeyError):
+            assert config['InvalidKey'] == "my_watch"
+
+    def test_GetValidKey_ConfigFileDoesNotContainThatKey_ReturnsDefaultValue(self, tmpdir):
+
+        config_file = tmpdir.join("config.ini")
+        config_file.write(
+            "[DEFAULT]\n"
+            "WatchDirectory = my_watch\n"
+        )
+
+        config = Configuration(str(config_file))
+
+        assert config['StorageDirectory'] == defaults.config['StorageDirectory']
+
+    def test_GetKey_ConfigFileDoesNotExist_ReturnDefaultValue(self, tmpdir):
+
+        config_file = tmpdir.join("config.ini")
+        config = Configuration(str(config_file))
+
+        assert config['WatchDirectory'] == defaults.config['WatchDirectory']
+
+    def test_SetValueToValidKey_ConfigFileExistsAndContainsKey_ConfigurationAndConfigFileAreUpdated(self, tmpdir):
+
+        config_file = tmpdir.join("config.ini")
+        config_file.write(
+            "[DEFAULT]\n"
+            "WatchDirectory = my_watch\n"
+        )
+        config = Configuration(str(config_file))
+
+        config['WatchDirectory'] = 'new_my_watch'
+
+        assert self.config_file_contains(str(config_file), 'WatchDirectory', 'new_my_watch')
+        assert config['WatchDirectory'] == 'new_my_watch'
+
+    def test_SetValueToValidKey_ConfigFileExistsButDoesNotContainsKey_KeyAndValueAreAddedToConfigFileIs(self, tmpdir):
+
+        config_file = tmpdir.join("config.ini")
+        config_file.write(
+            "[DEFAULT]\n"
+            "WatchDirectory = my_watch\n"
+        )
+        config = Configuration(str(config_file))
+
+        config['StorageDirectory'] = 'my_storage'
+
+        assert self.config_file_contains(str(config_file), 'StorageDirectory', 'my_storage')
+        assert config['StorageDirectory'] == 'my_storage'
+
+    def test_SetValueToValidKey_ConfigFileDoesNotExist_ConfigFileCreatedWithKeyAndValueSet(self, tmpdir):
+
+        config_file = tmpdir.join("config.ini")
+        config = Configuration(str(config_file))
+
+        config['WatchDirectory'] = 'my_watch'
+
+        assert self.config_file_contains(str(config_file), 'WatchDirectory', 'my_watch')
+        assert config['WatchDirectory'] == 'my_watch'
+
+    def test_SetValueToInvalidValidKey_RaisesKeyError(self, tmpdir):
+
+        config_file = tmpdir.join("config.ini")
+        config = Configuration(str(config_file))
+
+        with pytest.raises(KeyError):
+            config['InvalidKey'] = 'some value'
+
+    def test_SetValueToValidKey_DirectoryContainingConfigFIleDoesNotExist_RaisesFileNotFoundError(self, tmpdir):
+
+        config_file = tmpdir.join("some dir").join("config.ini")
+        config = Configuration(str(config_file))
+
+        with pytest.raises(FileNotFoundError):
+            config['WatchDirectory'] = 'my_watch'
+
+    def test_GetValueToValidKey_NoPermissionToReadConfigFile_RaisesOSError(self, tmpdir):
+
+        config_file = tmpdir.join("config.ini")
+        config_file.write(
+            "[DEFAULT]\n"
+            "WatchDirectory = my_watch\n"
+        )
+        config_file.chmod(mode=111)
+        config = Configuration(str(config_file))
+
+        with pytest.raises(OSError) as exception_info:
+            value = config['WatchDirectory']
+
+        assert "Permission denied" in str(exception_info.value)
+
+    def test_SetValueToValidKey_NoPermissionToWriteToConfigFile_RaisesOSError(self, tmpdir):
+
+        config_file = tmpdir.join("config.ini")
+        config_file.write("")
+        config_file.chmod(mode=555)
+        config = Configuration(str(config_file))
+
+        with pytest.raises(OSError) as exception_info:
+            config['WatchDirectory'] = 'my_watch'
+
+        assert "Permission denied" in str(exception_info.value)
+
+    def test_SetValueToValidKey_NoPermissionToWriteToConfigFileDirectory_RaisesOSError(self, tmpdir):
+
+        config_dir = tmpdir.mkdir("conf")
+        config_dir.chmod(mode=555)
+        config_file = config_dir.join("config.ini")
+        config = Configuration(str(config_file))
+
+        with pytest.raises(OSError) as exception_info:
+            config['WatchDirectory'] = 'my_watch'
+
+        assert "Permission denied" in str(exception_info.value)
