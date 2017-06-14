146ff73e3dbe3e5d134c8233bd5ffad6e6bdf442
David Fialho <fialho.david@protonmail.com>
2017-06-14 13:45:47 +0100

    Refactored the unit tests for entrypoint

--- episode_organizer/config_client/tests/unit/test_entrypoint.py
+++ episode_organizer/config_client/tests/unit/test_entrypoint.py
@@ -6,22 +6,23 @@ from episode_organizer.config_client import EntryPoint
 
 class TestEntrypoint:
 
+    @staticmethod
+    def set_input_args(args):
+        sys.argv = ['script']
+        sys.argv.extend(args)
+
     @patch.object(EntryPoint, 'setup_client')
     def test_GetValueWithoutSpecifyingTheHostOrPort_DefaultsToLocalhostAndPort8000(self, setup_client_mock):
 
-        sys.argv = ['script', '-g', 'WatchDirectory']
-
-        entrypoint = EntryPoint()
-        entrypoint.main()
+        self.set_input_args(['-g', 'WatchDirectory'])
+        EntryPoint().main()
 
         setup_client_mock.assert_called_once_with('localhost', '35121')
 
     @patch.object(EntryPoint, 'setup_client')
     def test_SetValueWithoutSpecifyingTheHostOrPort_DefaultsToLocalhostAndPort8000(self, setup_client_mock):
 
-        sys.argv = ['script', '-s', 'WatchDirectory', 'watch/']
-
-        entrypoint = EntryPoint()
-        entrypoint.main()
+        self.set_input_args(['-s', 'WatchDirectory', 'watch/'])
+        EntryPoint().main()
 
         setup_client_mock.assert_called_once_with('localhost', '35121')
