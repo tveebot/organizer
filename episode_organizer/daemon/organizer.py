bd8de88d04ec35c6cbfd137c168dc15a1ca0653e
David Fialho <fialho.david@protonmail.com>
2017-06-15 19:21:12 +0100

    Fix: log messages now show correct paths

--- episode_organizer/daemon/organizer.py
+++ episode_organizer/daemon/organizer.py
@@ -80,12 +80,12 @@ class Organizer(WatchHandler):
 
     def on_new_directory(self, dir_path):
         """ Called by the watcher if the service is running when a new directory is detected in the watch directory """
-        self.logger.debug("Detected new directory: %s" % os.path.relpath(self.watch_dir, dir_path))
+        self.logger.debug("Detected new directory: %s" % os.path.relpath(dir_path, self.watch_dir))
         self._handle_new_path(dir_path)
 
     def on_new_file(self, file_path):
         """ Called by the watcher if the service is running when a new file is detected in the watch directory """
-        self.logger.debug("Detected new file: %s" % os.path.relpath(self.watch_dir, file_path))
+        self.logger.debug("Detected new file: %s" % os.path.relpath(file_path, self.watch_dir))
         self._handle_new_path(file_path)
 
     def _handle_new_path(self, path):
@@ -95,9 +95,9 @@ class Organizer(WatchHandler):
         episode in the correct sub-directory in the storage directory.
         """
         try:
-            self.logger.info("Obtaining episode file from '%s'" % os.path.relpath(self.watch_dir, path))
+            self.logger.info("Obtaining episode file from '%s'" % os.path.relpath(path, self.watch_dir))
             episode_file = self.episode_filter.get_episode_file(path)
-            self.logger.info("Filter indicated the episode file is '%s'" % os.path.relpath(self.watch_dir, path))
+            self.logger.info("Filter indicated the episode file is '%s'" % os.path.relpath(path, self.watch_dir))
 
             self.logger.info("Obtaining episode information")
             episode = self.mapper.map_to_episode(os.path.basename(path))
@@ -105,11 +105,11 @@ class Organizer(WatchHandler):
 
             self.logger.info("Moving episode file to storage")
             self.storage_manager.store(episode_file, episode)
-            self.logger.info("Episode file stored in '%s'" % os.path.relpath(self.storage_manager.storage_dir, path))
+            self.logger.info("Episode file stored in '%s'" % os.path.relpath(path, self.storage_manager.storage_dir))
 
             if os.path.exists(path):
                 shutil.rmtree(path)
-                self.logger.info("Removed '%s' from watch directory" % os.path.relpath(self.watch_dir, path))
+                self.logger.info("Removed '%s' from watch directory" % os.path.relpath(path, self.watch_dir))
 
         except StorageError as error:
             self.logger.error(str(error))
