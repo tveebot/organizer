c50acd576b680a3d723d68c3a73b7c71542729f3
David Fialho <fialho.david@gmail.com>
2017-06-15 16:07:45 +0100

    Reimplemented the 'Configuration' class
    It is no longer associated with a config file
    Included new methods to load and save configurations from and to disk

--- episode_organizer/daemon/configuration.py
+++ episode_organizer/daemon/configuration.py
@@ -1,5 +1,4 @@
 from configparser import ConfigParser
-from contextlib import suppress
 
 from pkg_resources import resource_filename, Requirement
 
@@ -13,76 +12,22 @@ class Configuration:
     default_config_file = resource_filename(
         Requirement.parse("episode_organizer"), 'episode_organizer/daemon/default.ini')
 
-    def __init__(self, config_file):
-        """
-        Initializes the configurations with the default values from the 'default.ini' file.
-        If the provided config file exists then the configurations from that file will be used.
-        """
-        self.config_file = config_file
+    def __init__(self):
+        """ Initializes the configurations with the default values from the 'default.ini' file. """
         self._config = ConfigParser()
         self._config.read(self.default_config_file)
 
-    def __getitem__(self, key):
-        """
-        Returns the current value for a configuration key. If the configuration file does not exist, then returns the
-        default value for the given key.
-
-        :param key: the configuration key to obtain the value of.
-        :return: the value for the given key.
-        :raise PermissionError: if the given key is invalid.
-        :raise OSError:  if it fails to read the config file.
-        """
-        # Note: should not use read(file) below because this ignores any OS error when trying to read the file
-        # Here we do want to ignore FileNotFoundError, but not other OS errors such as permission denied or others.
-        # We want to make sure that if the file exists, then we are able to read it.
-        with suppress(FileNotFoundError):
-            with open(self.config_file) as file:
-                self._config.read_file(file)  # do not user read() here (see note above)
-
-        return self._config['DEFAULT'][key]
-
-    def __setitem__(self, key, value):
-        """
-        Sets a new value for a valid configuration key. Updates the configuration file.
-
-        :param key:                 configuration key to set value for.
-        :param value:               value to set.
-        :raise KeyError:            if the provided key is not valid.
-        :raise PermissionError: if it fails to write to the config file.
-        :raise FileNotFoundError: if it failed to create the config file because its directory does not exist.
-        """
-        if key not in self._config['DEFAULT']:
-            raise KeyError("Key '%s' is not a valid" % key)
-
-        previous_value = self._config['DEFAULT'][key]
-        self._config['DEFAULT'][key] = value
-
-        try:
-            self._save()
-        except:
-            # rollback to previous value
-            self._config['DEFAULT'][key] = previous_value
-            raise
-
-    def _save(self):
-
-        # Update the configuration file
-        with open(self.config_file, "w") as file:
-            self._config.write(file)
-
     @staticmethod
-    def from_dict(config_file, config: dict):
+    def from_dict(config: dict):
         """
         Builds a configuration instance from a dictionary with the configurations. The keys mut be valid, otherwise a
-        KeyError will be raised. The configuration is saved to the provided file. If the dictionary does not defines
-        some configuration parameter, then the default one is used.
+        KeyError will be raised. If the dictionary does not define some configuration parameter, then the default 
+        value is used.
 
-        :param config_file: the file to save configurations to.
-        :param config:      the dictionary defining the configurations.
+        :param config:  the dictionary defining the configurations.
         :return: a new configuration instance initialized with the configurations provided in the dictionary.
         """
-        configuration = Configuration(config_file)
-
+        configuration = Configuration()
         for key, value in config.items():
 
             if key not in configuration._config['DEFAULT']:
@@ -90,5 +35,76 @@ class Configuration:
 
             configuration._config['DEFAULT'][key] = value
 
-        configuration._save()
         return configuration
+
+    def load(self, config_file):
+        """
+        Loads the configuration file into memory. If the file does not exist, then it is ignored and the default values 
+        are kept.  
+        
+        :param config_file:         the configuration file to load configurations from.
+        :raise FileNotFoundError:   if the config file does not exist.
+        :raise PermissionError:     if the given key is invalid.
+        :raise OSError:             if it fails to read the config file.
+        """
+        with open(config_file) as file:
+            self.load_file(file)
+
+    def load_file(self, file):
+        """ The same as load but takes a file like object """
+        self._config.read_file(file)
+
+    def save(self, config_file):
+        """
+        Saves the current configuration in memory to the config file 
+
+        :param config_file:         the path to the file to save configurations to.
+        :raise KeyError:            if the provided key is not valid.
+        :raise PermissionError:     if it fails to write to the config file.
+        :raise FileNotFoundError:   if it failed to create the config file because its directory does not exist.
+        """
+        # Update the configuration file
+        with open(config_file, "w") as file:
+            self.save_file(file)
+
+    def save_file(self, file):
+        """ The same as save but takes a file like object """
+        self._config.write(file)
+
+    def __getitem__(self, key):
+        """
+        Returns the current value for a configuration key.
+
+        :param key:         the configuration key to obtain the value of.
+        :return:            the value for the given key.
+        :raises KeyError:   if the the given key is not valid.
+        """
+        return self._config['DEFAULT'][key]
+
+    def __setitem__(self, key, value):
+        """
+        Sets a new value for a valid configuration key. Updates the configuration file.
+
+        :param key:         configuration key to set value for.
+        :param value:       value to set.
+        :raise KeyError:    if the provided key is not valid.
+        """
+        if key not in self._config['DEFAULT']:
+            raise KeyError("Key '%s' is not a valid" % key)
+
+        self._config['DEFAULT'][key] = value
+
+    def update(self, key, value, config_file):
+        """
+        Calling this method is similar to calling __setitem__() followed by save(). An important difference is that 
+        if any error occurs while trying to save changes to the config file, then the configuration in memory is not 
+        changed as well.
+
+        :param key:                 the configuration key to set value for.
+        :param value:               the value to set.
+        :param config_file:         the file to save configurations to.
+        :raise KeyError:            if the provided key is not valid.
+        :raise PermissionError:     if it fails to write to the config file.
+        :raise FileNotFoundError:   if it failed to create the config file because its directory does not exist.
+        """
+        pass
--- episode_organizer/tests/unit/test_configuration.py
+++ episode_organizer/tests/unit/test_configuration.py
@@ -1,4 +1,6 @@
+import configparser
 from configparser import ConfigParser
+from io import StringIO
 
 import pytest
 
@@ -20,144 +22,147 @@ class TestConfiguration:
         except KeyError:
             return False
 
-    def test_GetValidKey_ReturnsCorrespondingValue(self, tmpdir):
+    def test_GetValue_ForValidKey_ReturnsCorrespondingValue(self):
 
-        config_file = tmpdir.join("config.ini")
-        config_file.write(
-            "[DEFAULT]\n"
-            "WatchDirectory = my_watch\n"
-        )
+        config = Configuration.from_dict({
+            'WatchDirectory': "test_watch"
+        })
 
-        config = Configuration(str(config_file))
+        assert config['WatchDirectory'] == "test_watch"
 
-        assert config['WatchDirectory'] == "my_watch"
+    def test_GetValue_ForInvalidKey_RaisesKeyError(self):
 
-    def test_GetInvalidValidKey_ReturnsKeyError(self, tmpdir):
-
-        config_file = tmpdir.join("config.ini")
-        config_file.write(
-            "[DEFAULT]\n"
-            "WatchDirectory = my_watch\n"
-        )
-
-        config = Configuration(str(config_file))
+        config = Configuration.from_dict({
+            'WatchDirectory': "test_watch"
+        })
 
         with pytest.raises(KeyError):
-            assert config['InvalidKey'] == "my_watch"
+            value = config['InvalidKey']
 
-    def test_GetValidKey_ConfigFileDoesNotContainThatKey_ReturnsDefaultValue(self, tmpdir):
+    def test_GetValue_ForValidKeyWithoutADefinedValue_ReturnsDefaultValueForThatKey(self):
 
-        config_file = tmpdir.join("config.ini")
-        config_file.write(
-            "[DEFAULT]\n"
-            "WatchDirectory = my_watch\n"
-        )
+        config = Configuration.from_dict({
+            'WatchDirectory': "test_watch"
+        })
 
-        config = Configuration(str(config_file))
-
+        # StorageDirectory is a valid key but is not defined
         assert config['StorageDirectory'] == defaults.config['StorageDirectory']
 
-    def test_GetKey_ConfigFileDoesNotExist_ReturnDefaultValue(self, tmpdir):
+    def test_SetValue_ForValidKey_ValueUpdatedForThatKey(self):
 
-        config_file = tmpdir.join("config.ini")
-        config = Configuration(str(config_file))
+        config = Configuration()
+        config['WatchDirectory'] = "new_watch"
+
+        assert config['WatchDirectory'] == "new_watch"
+
+    def test_SetValue_ForInvalidKey_RaisesKeyError(self):
+
+        config = Configuration()
+
+        with pytest.raises(KeyError):
+            config['InvalidKey'] = "new_watch"
+
+    def test_Load_EmptyConfigFile_UsesDefaultValues(self):
+
+        config_file = StringIO("")
+
+        config = Configuration()
+        config.load_file(config_file)
 
         assert config['WatchDirectory'] == defaults.config['WatchDirectory']
 
-    def test_SetValueToValidKey_ConfigFileExistsAndContainsKey_ConfigurationAndConfigFileAreUpdated(self, tmpdir):
+    def test_Load_FileWhichDefinesValuesForAKey_ConfigUsesProvidedValueForThatKey(self):
 
-        config_file = tmpdir.join("config.ini")
-        config_file.write(
+        config_file = StringIO(
             "[DEFAULT]\n"
-            "WatchDirectory = my_watch\n"
+            "WatchDirectory = test_watch\n"
         )
-        config = Configuration(str(config_file))
 
-        config['WatchDirectory'] = 'new_my_watch'
+        config = Configuration()
+        config.load_file(config_file)
 
-        assert self.config_file_contains(str(config_file), 'WatchDirectory', 'new_my_watch')
-        assert config['WatchDirectory'] == 'new_my_watch'
+        assert config['WatchDirectory'] == "test_watch"
 
-    def test_SetValueToValidKey_ConfigFileExistsButDoesNotContainsKey_KeyAndValueAreAddedToConfigFileIs(self, tmpdir):
+    def test_Load_FileDoesNotExist_RaisesFileNotFoundError(self, tmpdir):
 
-        config_file = tmpdir.join("config.ini")
-        config_file.write(
-            "[DEFAULT]\n"
-            "WatchDirectory = my_watch\n"
-        )
-        config = Configuration(str(config_file))
-
-        config['StorageDirectory'] = 'my_storage'
-
-        assert self.config_file_contains(str(config_file), 'StorageDirectory', 'my_storage')
-        assert config['StorageDirectory'] == 'my_storage'
-
-    def test_SetValueToValidKey_ConfigFileDoesNotExist_ConfigFileCreatedWithKeyAndValueSet(self, tmpdir):
-
-        config_file = tmpdir.join("config.ini")
-        config = Configuration(str(config_file))
-
-        config['WatchDirectory'] = 'my_watch'
-
-        assert self.config_file_contains(str(config_file), 'WatchDirectory', 'my_watch')
-        assert config['WatchDirectory'] == 'my_watch'
-
-    def test_SetValueToInvalidValidKey_RaisesKeyError(self, tmpdir):
-
-        config_file = tmpdir.join("config.ini")
-        config = Configuration(str(config_file))
-
-        with pytest.raises(KeyError):
-            config['InvalidKey'] = 'some value'
-
-    def test_SetValueToValidKey_DirectoryContainingConfigFileDoesNotExist_RaisesFileNotFoundErrorAndConfigNotChanged(
-            self, tmpdir):
-
-        config_file = tmpdir.join("some dir").join("config.ini")
-        config = Configuration(str(config_file))
-        previous_value = defaults.config['WatchDirectory']
+        config = Configuration()
 
         with pytest.raises(FileNotFoundError):
-            config['WatchDirectory'] = 'my_watch'
+            config.load(str(tmpdir.join("config.ini")))
 
-        assert config['WatchDirectory'] == previous_value
-
-    def test_GetValueToValidKey_NoPermissionToReadConfigFile_RaisesOSError(self, tmpdir):
-
-        config_file = tmpdir.join("config.ini")
-        config_file.write(
-            "[DEFAULT]\n"
-            "WatchDirectory = my_watch\n"
-        )
-        config_file.chmod(mode=0o333)
-        config = Configuration(str(config_file))
-
-        with pytest.raises(PermissionError):
-            value = config['WatchDirectory']
-
-    def test_SetValueToValidKey_NoPermissionToWriteToConfigFile_RaisesPermissionErrorAndConfigNotChanged(self, tmpdir):
+    def test_Load_DoesNotHavePermissionToReadFile_RaisesPermissionError(self, tmpdir):
 
         config_file = tmpdir.join("config.ini")
         config_file.write("")
-        config_file.chmod(mode=0o555)
-        config = Configuration(str(config_file))
-        previous_value = defaults.config['WatchDirectory']
+        config_file.chmod(0o333)
 
         with pytest.raises(PermissionError):
-            config['WatchDirectory'] = 'my_watch'
+            Configuration().load(str(config_file))
 
-        assert config['WatchDirectory'] == previous_value
+    def test_Load_FileFormatIsWrong_RaisesConfigParserError(self):
 
-    def test_SetValueToValidKey_NoPermissionToWriteToConfigFileDirectory_RaisesPermissionErrorAndConfigNotChanged(
-            self, tmpdir):
+        config_file = StringIO("invalid format\n")
+
+        with pytest.raises(configparser.Error):
+            Configuration().load_file(config_file)
+
+    def test_Save_ToExistingFile_FileContainsNewConfigurationsIncludingDefault(self, tmpdir):
+
+        config_file = tmpdir.join("config.ini")
+        config_file.write(
+            "[DEFAULT]\n"
+            "WatchDirectory = test_watch\n"
+        )
+        config = Configuration.from_dict({
+            'WatchDirectory': 'test_watch'
+        })
+
+        config.save(str(config_file))
+
+        assert self.config_file_contains(str(config_file), 'WatchDirectory', 'test_watch')
+        assert self.config_file_contains(str(config_file), 'StorageDirectory', defaults.config['StorageDirectory'])
+
+    def test_Save_ToNonExistingFile_FileIsCreatedAndConfigsSaved(self, tmpdir):
+
+        config_file = tmpdir.join("config.ini")
+        config = Configuration.from_dict({
+            'WatchDirectory': 'test_watch'
+        })
+
+        config.save(str(config_file))
+
+        assert self.config_file_contains(str(config_file), 'WatchDirectory', 'test_watch')
+
+    def test_Save_ToFileDirectoryOfWhichDoesNotExist_RaisesFileNotFoundError(self, tmpdir):
+
+        config_file = tmpdir.join("conf").join("config.ini")
+        config = Configuration.from_dict({
+            'WatchDirectory': 'test_watch'
+        })
+
+        with pytest.raises(FileNotFoundError):
+            config.save(str(config_file))
+
+    def test_Save_ToExistingFileWithoutWritePermission_RaisesPermissionError(self, tmpdir):
+
+        config_file = tmpdir.join("config.ini")
+        config_file.write("")
+        config_file.chmod(0o555)
+        config = Configuration.from_dict({
+            'WatchDirectory': 'test_watch'
+        })
+
+        with pytest.raises(PermissionError):
+            config.save(str(config_file))
+
+    def test_Save_ToNonExistingFileInDirectoryWithoutWritePermission_RaisesPermissionError(self, tmpdir):
 
         config_dir = tmpdir.mkdir("conf")
-        config_dir.chmod(mode=0o555)
         config_file = config_dir.join("config.ini")
-        config = Configuration(str(config_file))
-        previous_value = defaults.config['WatchDirectory']
+        config_dir.chmod(0o555)
+        config = Configuration.from_dict({
+            'WatchDirectory': 'test_watch'
+        })
 
         with pytest.raises(PermissionError):
-            config['WatchDirectory'] = 'my_watch'
-
-        assert config['WatchDirectory'] == previous_value
+            config.save(str(config_file))
