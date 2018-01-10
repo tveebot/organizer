84f9ec990bdc6b048c18b6ed36089594852375ca
David Fialho <fialho.david@protonmail.com>
2018-01-10 08:48:52 +0000

    Add 'wait()' method to watcher

--- tveebot_organizer/watcher.py
+++ tveebot_organizer/watcher.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 from pathlib import Path
 
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
@@ -35,6 +36,11 @@ class Watcher:
         # This watch is used when changing the current watch directory
         self._last_watch: ObservedWatch = None
 
+        # Event to indicate the main loop exited
+        # Initially the event must be set
+        self._exited = threading.Event()
+        self._exited.set()
+
     @property
     def watch_dir(self) -> Path:
         return self._watch_dir
@@ -56,12 +62,30 @@ class Watcher:
 
     def run_forever(self):
         """ Runs the watching until the *shutdown()* is called """
-        self._observer.start()
-        self._last_watch = self._observer.schedule(Watcher.Handler(self), str(self.watch_dir))
-        self._observer.join()
-        # self._observer.unschedule_all()
-        self._last_watch = None
+        self._exited.clear()
+        try:
+            self._observer.start()
+            self._last_watch = self._observer.schedule(Watcher.Handler(self), str(self.watch_dir))
+            self._observer.join()
+            self._observer.unschedule_all()
+            self._last_watch = None
+        finally:
+            self._exited.set()
 
     def shutdown(self):
         """ Tells the loop in *run_forever()* to stop """
         self._observer.stop()
+
+    def wait(self, timeout: float = None) -> bool:
+        """
+        Blocks while the watcher is running. If the watcher is not running it returns immediately.
+        Otherwise, blocks until the watcher shuts down or until the *timeout* occurs.
+
+        When the timeout argument is present and not None, it should be a floating point number
+        specifying a timeout in seconds (or fractions thereof).
+
+        This method returns true if and only if the internal flag has been set to true,
+        either before the wait call or after the wait starts, so it will always return True
+        except if a timeout is given and the operation times out.
+        """
+        return self._exited.wait(timeout)
